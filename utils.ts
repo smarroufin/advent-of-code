cb854a587a99a6f267b82b38e71f792c12aede88
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2025-12-01 19:44:13 +0100

    Initial commit

---
+++ utils.ts
@@ -0,0 +1,23 @@
+import { readFile } from 'node:fs/promises'
+
+async function readInput() {
+  const filename = process.argv[2]
+  if (!filename) {
+    throw new Error("No input file specified")
+  }
+  try {
+    return await readFile(filename, "utf8")
+  } catch (err) {
+    throw new Error(`Error reading input file: ${err}`)
+  }
+}
+
+export function run(fn: (input: string) => Promise<void>) {
+  readInput()
+    .then(input => fn(input))
+    .catch(err => {
+      console.error(err.message)
+      process.exit(1)
+    })
+}
+
